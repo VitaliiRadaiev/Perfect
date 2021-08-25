b8af0e61b9986825545b411abe57eb93d95d08b9
Vitalii Radaiev <hf.chelenger@gamil.com>
2021-08-25 12:26:11 +0300

    fix

--- dist/js/app.js
+++ dist/js/app.js
@@ -1206,39 +1206,47 @@ if(cardDetailItems.length) {
     })
 };
 	{
-	const slider = document.querySelector('.logos-list');
-	if(slider) {
-		let mySwiper;
-
-		function mobileSlider() {
-			if(document.documentElement.clientWidth <= 767 && slider.dataset.mobile == 'false') {
-				mySwiper = new Swiper(slider, {
-					slidesPerView: 3,
-					speed: 600,
-					pagination: {
-					    el: slider.querySelector('.swiper-pagination'),
-					    clickable: true,
-					  },
-				});
-
-				slider.dataset.mobile = 'true';
-
-				//mySwiper.slideNext(0);
-			}
-
-			if(document.documentElement.clientWidth > 767) {
-				slider.dataset.mobile = 'false';
-
-				if(slider.classList.contains('swiper-container-initialized')) {
-					mySwiper.destroy();
+	const sliders = document.querySelectorAll('.logos-list');
+	if(sliders.length) {
+		sliders.forEach(slider => {
+			if(slider) {
+				let mySwiper;
+		
+				function mobileSlider() {
+					if(document.documentElement.clientWidth <= 767 && slider.dataset.mobile == 'false') {
+						mySwiper = new Swiper(slider, {
+							autoplay: {
+								delay: 3000,
+								disableOnInteraction: false,
+							},
+							slidesPerView: 3,
+							speed: 600,
+							pagination: {
+								el: slider.querySelector('.swiper-pagination'),
+								clickable: true,
+							  },
+						});
+		
+						slider.dataset.mobile = 'true';
+		
+						//mySwiper.slideNext(0);
+					}
+		
+					if(document.documentElement.clientWidth > 767) {
+						slider.dataset.mobile = 'false';
+		
+						if(slider.classList.contains('swiper-container-initialized')) {
+							mySwiper.destroy();
+						}
+					}
 				}
+		
+				mobileSlider();
+		
+				window.addEventListener('resize', () => {
+					mobileSlider();
+				})
 			}
-		}
-
-		mobileSlider();
-
-		window.addEventListener('resize', () => {
-			mobileSlider();
 		})
 	}
 
